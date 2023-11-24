4b1bbdaf4bed5557cb53cdcdc26c02c793db485d
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-11-23 22:12:53 -0600

    Change Element List Name
    Changed name of HomeBox component to HomeElement component

--- Front/React/reed-family/src/App.js
+++ Front/React/reed-family/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-const boxList = [
+const elementList = [
   {id: 1, name: "Pictures"}, // TODO: add picture list to object to display
   {id: 2, name: "Recipes"}, // TODO: add recipe list to object to display
 ];
@@ -10,19 +10,19 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Reed Family!</h1>
-      <div className="HomeBoxList">
-        {boxList.map(box => (
-          <HomeBox key={box.id} box={box} />
+      <div className="HomeElementList">
+        {elementList.map(element => (
+          <HomeElement key={element.id} element={element} />
         ))}
       </div>
     </div>
   );
 }
 
-function HomeBox({box}) {
+function HomeElement({element}) {
   return (
-    <div className="HomeBox">
-      <h2>{box.name}</h2>
+    <div className="HomeElement">
+      <h2>{element.name}</h2>
     </div>
   );
 }
