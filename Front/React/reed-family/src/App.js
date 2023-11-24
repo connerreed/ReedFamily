fa6460f990a26e6fb5afa9c2659a0935ad4ba841
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-11-24 16:41:33 -0600

    Added cycle functionality to preview pictures
    Added state to HomeElement component to allow update of current image. Cycles through list of pictures until reaches end of list, then wraps back to the beginning.

--- Front/React/reed-family/src/App.js
+++ Front/React/reed-family/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 const pictureList = [
   {id: 1, src: "./imgs/Pictures/FamilyPhoto1.png"},
@@ -31,14 +32,26 @@ function App() {
   );
 }
 
-function HomeElement({element}) {
-  let currPictureList = element.list;
-  let currPictureSrc = currPictureList[0].src;
+function HomeElement({ element }) {
+  const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentPictureIndex(prevIndex => 
+        prevIndex === element.list.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change image every 3000 milliseconds (3 seconds)
+
+    return () => clearInterval(timer); // Clean up the interval on component unmount
+  }, [element.list]);
+
+  const currentPictureSrc = require(`${element.list[currentPictureIndex].src}`);
+
   return (
     <a href={element.link}>
       <div className="HomeElement">
-      <img src={require(`${currPictureSrc}`)} alt="Preview Photo" style={{ width: '100px', height: '100px' }} />
-      <h2>{element.name}</h2>
+        <img src={currentPictureSrc} alt="Preview" style={{ width: '100px', height: '100px' }} />
+        <h2>{element.name}</h2>
       </div>
     </a>
   );
