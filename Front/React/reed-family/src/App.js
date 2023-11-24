785b5b453d1ab53ac32ba3ac8b8639bc26b02ed0
Conner Reed <98499861+connerreed@users.noreply.github.com>
2023-11-23 21:43:29 -0600

    Update App.js
    Setup dynamic homepage box list. Box data can be changed with boxList, and display of boxes can be changed with HomeBox component.

--- Front/React/reed-family/src/App.js
+++ Front/React/reed-family/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const boxList = [
+  {id: 1, name: "Pictures"}, // TODO: add picture list to object to display
+  {id: 2, name: "Recipes"}, // TODO: add recipe list to object to display
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Reed Family!</h1>
+      <div className="HomeBoxList">
+        {boxList.map(box => (
+          <HomeBox key={box.id} box={box} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function HomeBox({box}) {
+  return (
+    <div className="HomeBox">
+      <h2>{box.name}</h2>
     </div>
   );
 }
